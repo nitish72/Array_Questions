beffa3cf1c66c747924936a83ff9bf7a0bf722d2
NItish Kumar <75878650+nitish72@users.noreply.github.com>
2023-10-06 22:17:55 +0530

    Update pyramid_.java

--- pyramid_.java
+++ pyramid_.java
@@ -1,10 +1,15 @@
-Pyamid Patterns
-
+com.Pyamid_Patterns
+class Pyaramid{
+  public static void main(String args[]){
+  
 int i,j;
 fot(row=1;row<=5;row++)
 {
   for(col=1;col<=row;col++)
 {
-
-}
+   System.out.print("*);
+  }
+  System.out.println( );
+} 
+  }
 }
