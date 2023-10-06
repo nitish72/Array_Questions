4ea917a133b8a029c7f0a9b9816b43060188d86f
NItish Kumar <75878650+nitish72@users.noreply.github.com>
2023-10-06 23:40:15 +0530

    Update Star_Pyramid_with_Reverse.java

--- Star_Pyramid_with_Reverse.java
+++ Star_Pyramid_with_Reverse.java
@@ -1,4 +1,19 @@
 class Star_Pyramid
 {
-
  
+public static void main(String args[]) {
+
+ for(int row=1;row<=5;row++){
+     for(int col=1;col<=5;col++){
+      System.out.print("*");
+     }
+   System.out.println();
+     }
+    for(int row=1;row<=5;row++){
+       for(int col=5;col>=row;col--) 
+        {
+         System.out.print("*");
+        }
+     System.out.println();
+    }
+ }
